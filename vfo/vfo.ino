3c250da692712935919d8de7039f31f5cca660bd
Pat Hanrahan <hanrahan@cs.stanford.edu>
2015-03-28 11:18:11 -0700

    Converted vfo to use u8glib

--- vfo/vfo.ino
+++ vfo/vfo.ino
@@ -1,14 +1,12 @@
-// oneMHz
-// 
 // Demonstrate use of the DDS Library
 // m0xpd
 // http://m0xpd.blogspot.com
 // March 2014
 
-// include the DDS Library:
-#include <Wire.h>
-#include <SeeedOLED.h>
+#include "U8glib.h"
 #include <DDS.h>
+
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST);
 
 //======================================
 // AD9850 Module....
@@ -34,19 +32,33 @@ void setFrequency()
 {
   dds.setFrequency(freq);  
   
-  SeeedOled.clearDisplay();  
-  SeeedOled.setTextXY(2,5);
-  SeeedOled.putNumber(freq); 
+  u8g.firstPage();  
+  do {
+      u8g.setPrintPos( 10, 22 );
+      u8g.print(freq);
+  } while( u8g.nextPage() ); 
 }
 
 
 void setup() {
-  Wire.begin();
-  SeeedOled.init();  //initialze SEEED OLED display
   DDRB|=0x21;        
   PORTB |= 0x21;
-  SeeedOled.setNormalDisplay();      //Set display to normal mode (i.e non-inverse mode)
-  SeeedOled.setPageMode();           //Set addressing mode to Page Mode
+  
+    // assign default color value
+  if ( u8g.getMode() == U8G_MODE_R3G3B2 ) {
+    u8g.setColorIndex(255);     // white
+  }
+  else if ( u8g.getMode() == U8G_MODE_GRAY2BIT ) {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if ( u8g.getMode() == U8G_MODE_BW ) {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if ( u8g.getMode() == U8G_MODE_HICOLOR ) {
+    u8g.setHiColorByRGB(255,255,255);
+  }
+  u8g.setFont(u8g_font_unifont);
+  u8g.setRot180();
  
   dds.init();  
   dds.trim(124997000);   // (Optional) trim if your xtal is not at 125MHz...
