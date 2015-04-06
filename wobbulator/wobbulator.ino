126ab768dc3d5b0b90726fe8d755e81304d60b43
Pat Hanrahan <hanrahan@cs.stanford.edu>
2015-04-05 18:17:33 -0700

    First check in

---
+++ wobbulator/wobbulator.ino
@@ -0,0 +1,32 @@
+#include <DDS.h>
+
+// AD9850 Pins
+const int W_CLK = 2;
+const int FQ_UD = 3; 
+const int DATA  = 4;
+const int RESET = 5;
+
+DDS dds(W_CLK, FQ_UD, DATA, RESET);
+
+const int sfreq = 7300000;
+const int efreq = 7400000;
+const int dfreq =    1000;
+
+void setup() {
+  Serial.begin(9600);
+
+  dds.init();  
+  dds.trim(124997000);   // (Optional) trim if your xtal is not at 125MHz...
+}
+
+void loop() {
+  for( int freq = sfreq; freq < efreq; freq += dfreq ) {
+      dds.setFrequency(freq);  
+      int response = analogRead(A0);
+      Serial.print('freq');
+      Serial.print(freq);
+      Serial.print('response');
+      Serial.print(response);
+      delay(10);
+  }
+}
