503b8d115d2f875f17a9fee91e38a5505fc7885d
phanrahan <hanrahan@cs.stanford.edu>
2017-01-01 11:14:21 -0800

    Updated arduino for spislave device on an fpga.

---
+++ spislave/spi.ino
@@ -0,0 +1,37 @@
+#include "SPI.h" 
+
+int ss=10; 
+
+const int DELAY=200
+
+
+
+; 
+ 
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ss, OUTPUT); 
+  SPI.begin(); 
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV16); // 1Mhz
+}
+ 
+int setValue(int value)
+{
+  digitalWrite(ss, LOW);
+  int result = SPI.transfer(value);
+  digitalWrite(ss, HIGH);
+  return result;
+}
+ 
+void loop()
+{
+  for (int i=0; i<256; i++) {
+    int result = setValue(i);
+    Serial.print(result);
+    Serial.println();
+    //delayMicroseconds(DELAY);
+    delay(DELAY);
+  }
+}
