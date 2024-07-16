b3e883ea658405e9f9a5a81d1e04e1f4a11f262c
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-07-16 12:19:27 -0700

    Update blink

--- xiao/esp32c3/blink/blink.ino
+++ xiao/esp32c3/blink/blink.ino
@@ -11,5 +11,5 @@ void loop() {
   digitalWrite(led, HIGH);   // turn the LED on 
   delay(1000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off
-  delay(1000);               // wait for a second
+  delay(200);               // wait for a second
 }
