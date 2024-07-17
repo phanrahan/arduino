df041a06dde4f7880c185e00de35c4b9f56d5706
phanrahan <myemail@stanford.edu>
2024-07-17 07:57:17 -0700

    More esp32s3 examples

---
+++ xiao/esp32c3/conn/conn.ino
@@ -0,0 +1,32 @@
+#include <WiFi.h>
+
+const char* ssid     = "your-ssid";
+const char* password = "your-password";   
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}  
+void loop()
+{
+}
