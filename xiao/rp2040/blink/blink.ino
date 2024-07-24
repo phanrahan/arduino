faffa3e4a2bb1886a3ea3644829db04a445a79b1
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-07-23 17:13:51 -0700

    Add rp2040 blink

---
+++ xiao/rp2040/blink/blink.ino
@@ -0,0 +1,15 @@
+// define led according to pin diagram
+//
+// https://wiki.seeedstudio.com/XIAO_ESP32C3_Getting_Started/
+int led = 25;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on 
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off
+  delay(200);               // wait for a second
+}
