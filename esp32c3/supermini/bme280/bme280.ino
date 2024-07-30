b306b565c5562d14d9c817f53896fa506df2fe03
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-07-30 15:25:05 -0700

    Working bme280 web server

--- esp32c3/supermini/bme280/bme280.ino
+++ esp32c3/supermini/bme280/bme280.ino
@@ -10,7 +10,7 @@ Adafruit_BME280 bme; // I2C
 
 const String ssid("Wifihill");
 const String password("Wifihill");
-//const IPAddress ip(10, 0, 0, 40);
+const IPAddress ip(10, 0, 0, 243);
 //const char* const hostname("esp32c3");
 
 WiFiServer server(80);
@@ -18,7 +18,7 @@ WiFiServer server(80);
 void setup() {
   Serial.begin(115200);
 
-  bool status = bme.begin(0x76);  
+  bool status = bme.begin();  
   if (!status) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);
@@ -27,7 +27,7 @@ void setup() {
   // Connect to WiFi.
   Serial.println("Connecting to " + ssid);
   WiFi.mode(WIFI_STA);
-  //WiFi.config(ip);  // Force this static IP address.
+  WiFi.config(ip);  // Force this static IP address.
   //WiFi.setHostname(hostname);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -69,6 +69,10 @@ void processRequest(const String& s) {
 }
 
 void respond(WiFiClient& client) {
+  int temperature = bme.readTemperature();
+  int pressure = bme.readPressure();
+  int humidity = bme.readHumidity();
+  int altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
   client.println("HTTP/1.1 200 OK");
   client.println("Content-type:text/html");
   client.println("Connection: close");
@@ -79,9 +83,22 @@ void respond(WiFiClient& client) {
     client.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
   client.println("</head>");
   client.println("<body>");
-    client.println("<p>BME280 temperature ");
-      client.print(bme.readTemperature());
+    client.println("<p>Temperature ");
+      client.print(temperature);
       client.println("C");
+    client.println("</p>");
+    client.println("<p>Pressure ");
+      client.print(pressure);
+      client.println("Pa");
+    client.println("</p>");
+        client.println("<p>Humidity ");
+      client.print(humidity);
+      client.println("%");
+    client.println("</p>");
+    client.println("</p>");
+        client.println("<p>Altitude ");
+      client.print(altitude);
+      client.println("m");
     client.println("</p>");
   client.println("</body>");
   client.println("</html>");
--- esp32c3/xiao/client/client.ino
+++ esp32c3/xiao/client/client.ino
@@ -1,4 +1,5 @@
 #include <WiFi.h>
+#include <esp_wifi.h>
 #include <HTTPClient.h>
 
 const char* ssid = "Wifihill";
@@ -10,6 +11,18 @@ const char* serverName = "https://10.0.0.132:8000/sensor/";
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
 
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -19,6 +32,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
+  readMacAddress();
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
