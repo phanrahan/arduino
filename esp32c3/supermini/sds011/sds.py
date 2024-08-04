1bb70cc016c33ae93f5eaa94f88be533f8ae7472
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-08-03 18:58:11 -0700

    Added doc and control fan

--- esp32c3/supermini/sds011/sds.py
+++ esp32c3/supermini/sds011/sds.py
@@ -1,4 +1,10 @@
+import time
 from sds011 import SDS011
 
 sensor = SDS011("/dev/cu.usbserial-143340", use_query_mode=True)
-print(sensor.query())  # Gets (pm25, pm10)
+sensor.sleep(sleep=False)  # Turn on fan and diode
+time.sleep(15)  # Allow time for the sensor to measure properly
+pm25, pm10 = sensor.query()
+print("pm25", pm25)
+print("pm10", pm10)
+sensor.sleep();
