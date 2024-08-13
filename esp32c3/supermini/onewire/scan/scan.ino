bda1ce873495ba68051c7dee5b537839f1932705
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-08-12 20:06:25 -0700

    Updated scan.ino

--- esp32c3/supermini/onewire/scan/scan.ino
+++ esp32c3/supermini/onewire/scan/scan.ino
@@ -1,92 +1,59 @@
-/*
-  Arduino One Wire Search
-  =======================
-  
-  Processing code for Ardunio to search for 1-wire devices and outputs address in hex to serial.
-  
-  - Adapted by Gordon Turner from code by hacktronics:
-    http://www.hacktronics.com/Tutorials/arduino-1-wire-address-finder.html
- 
-  - 1-Wire temperature sensor attached to digital pin 3.
-  
-  - Requires 1-wire Ardunio Library and Dallas Temperature Control Arduino Library.
-  - Included in Libraries folder or online:
-      http://www.pjrc.com/teensy/arduino_libraries/OneWire.zip
-      http://milesburton.com/Dallas_Temperature_Control_Library#Latest
- 
-  - Sample serial output:
-
-  Searching for 1-wire devices.
-  Found 1-Wire device with address: 0x28, 0x85, 0x95, 0x3A, 0x04, 0x00, 0x00, 0xB7
-  No more devices found.
-
- 
-*/
-
 #include <OneWire.h>
 
-// 1-Wire data wire is attached to ditigal pin 3.
-#define ONE_WIRE_BUS 10
+// OneWire DS18S20, DS18B20, DS1822 Temperature Example
+//
+// http://www.pjrc.com/teensy/td_libs_OneWire.html
+//
+// The DallasTemperature library can do all this work for you!
+// https://github.com/milesburton/Arduino-Temperature-Control-Library
 
-// Intialize an 1-Wire instance.
-OneWire oneWire(ONE_WIRE_BUS);
+OneWire  ds(10);  // on pin 10 (a 4.7K resistor is necessary)
 
-void setup(void) 
-{
-  // Open serial communications and wait for port to open.
+void setup(void) {
   Serial.begin(115200);
-  
+}
+
+void loop(void) {
   byte i;
   byte present = 0;
-  byte data[12];
+  byte data[9];
   byte addr[8];
+  float celsius, fahrenheit;
   
-  Serial.println("Searching for 1-wire devices.");
-  
-  while( oneWire.search(addr) ) 
-  {
-    // TODO: Test with more then one 1-wire device.
-  
-    Serial.print("Found 1-Wire device with address: ");
-  
-    for( i = 0; i < 8; i++) 
-    {
-      Serial.print("0x");
-      
-      if (addr[i] < 16) 
-      {
-        Serial.print('0');
-      }
-      
-      Serial.print(addr[i], HEX);
-      
-      if (i < 7) 
-      {
-        Serial.print(", ");
-      }
-      else
-      {
-        Serial.println();
-      }
-    }
-    
-    if ( OneWire::crc8( addr, 7) != addr[7]) 
-    {
-        Serial.println("Error: CRC is invalid.");
-        return;
-    }
+  if ( !ds.search(addr)) {
+    Serial.println("No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
   }
   
-  Serial.println("No more devices found.");
-  
-  oneWire.reset_search();
-  return;
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+      Serial.println("CRC is not valid!");
+      return;
+  }
+  Serial.println();
+ 
+  // the first ROM byte indicates which chip
+  switch (addr[0]) {
+    case 0x10:
+      Serial.println("  Chip = DS18S20");  // or old DS1820
+      break;
+    case 0x28:
+      Serial.println("  Chip = DS18B20");
+      break;
+    case 0x22:
+      Serial.println("  Chip = DS1822");
+      break;
+    default:
+      Serial.println("  Unrecognizable device.");
+      return;
+  } 
+
 }
-
-
-void loop()
-{
-  // Empty
-}
-
-
