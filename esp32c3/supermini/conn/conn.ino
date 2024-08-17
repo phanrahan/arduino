f4c8c6e44187935e203d1036242445628d153b5a
Pat Hanrahan <hanrahan@cs.stanford.edu>
2024-08-17 10:57:25 -0700

    Update conn.ino

--- esp32c3/supermini/conn/conn.ino
+++ esp32c3/supermini/conn/conn.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 
-const char* ssid     = "Wifihill";
-const char* password = "Wifihill";   
+const char* ssid = "Wifihill";
+const char* password = "Wifihill";
 char hostname[100];
 
 void create_hostname() {
@@ -10,30 +10,33 @@ void create_hostname() {
   snprintf(hostname, 100, "esp32c3-%02X%02X%02X", mac[3], mac[4], mac[5]);
 }
 
-void setup()
-{
-    Serial.begin(115200);
-    delay(10);
+void setup() {
+  Serial.begin(115200);
+  while(!Serial) ;
 
-    //create_hostname();
+  create_hostname();
 
-    Serial.println();
-    Serial.printf("Connecting to %s\n", ssid);
-    //Serial.printf("Connecting to %s as %s\n", ssid, hostname);
+  Serial.println();
+  Serial.printf("Connecting to %s as %s\n", ssid, hostname);
 
-    //WiFi.mode(WIFI_STA);
-    //WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
-    //WiFi.setHostname(hostname);
-    WiFi.begin(ssid, password);
+  WiFi.mode(WIFI_STA);
+  WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+  // WiFi.setHostname(hostname);
+  WiFi.begin(ssid, password);
+  //Serial.printf("Tx power %d\n", WiFi.getTxPower());
+  // WIFI_POWER_15dBm, WIFI_POWER_8_5dBm, WIFI_POWER_5dBm
+  WiFi.setTxPower(WIFI_POWER_19_5dBm);
+  //WiFi.setTxPower(WIFI_POWER_15dBm);
+  //WiFi.setTxPower(WIFI_POWER_8_5dBm);
+  //WiFi.setTxPower(WIFI_POWER_5dBm);
 
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
 
-    Serial.println(WiFi.localIP());
-}  
+  Serial.println(WiFi.localIP());
+}
 
-void loop()
-{
+void loop() {
 }
