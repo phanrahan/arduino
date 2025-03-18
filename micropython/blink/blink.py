a6bedd1fd84dd684a508e8771ba5f386fdaff6cb
Pat Hanrahan <hanrahan@cs.stanford.edu>
2025-03-17 17:51:23 -0700

    Added upy and ws2812

--- micropython/blink/blink.py
+++ micropython/blink/blink.py
@@ -2,7 +2,8 @@ from machine import Pin
 import time
 led_pin = 10  
 
-led = Pin(led_pin, Pin.OUT)
+#led = Pin(led_pin, Pin.OUT)
+led = Pin(7, Pin.OUT)  # Use the correct GPIO number instead of D10
 while 1:
   led.on()
   time.sleep_ms(500)
